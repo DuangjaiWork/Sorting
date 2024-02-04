beca8bf6644f7e3fe81e46763c5535ac5e72160a
DuangjaiWork <158825297+DuangjaiWork@users.noreply.github.com>
2024-02-05 04:35:16 +0700

    Add files via upload

--- MergeSort.py
+++ MergeSort.py
@@ -6,10 +6,10 @@ from pyglet import clock
 
 class Renderer(Window):
     def __init__(self):
-        super().__init__(790, 820, "Merge Sort")
+        super().__init__(960, 750, "Merge Sort")
         self.batch = Batch()
-        self.x = [3, 4, 2, 1, 6, 5]
-        self.bars = [Rectangle(100 + e * 100, 100, 80, i * 100, batch=self.batch) for e, i in enumerate(self.x)]
+        self.x = [3, 4, 2, 7, 1, 6, 5, 8]
+        self.bars = [Rectangle(100 + e * 100, 60, 60, i*80, batch=self.batch, color=(255, 255, 255, 255)) for e, i in enumerate(self.x)]
         self.animation_generator = self.merge_sort_animation(0, len(self.x) - 1)
 
     def merge_sort_animation(self, l, r):
@@ -23,17 +23,12 @@ class Renderer(Window):
         left, right = self.x[l:mid + 1], self.x[mid + 1:r + 1]
         i, j, k = 0, 0, l
         while i < len(left) and j < len(right):
-            self.x[k] = min((left[i], right[j]))
-            (i, j)[self.x[k] == left[i]] += 1
-            k += 1
+            self.bars[l + i].color = (255, 0, 0, 255)  # Set color to red for comparison
+            self.bars[mid + 1 + j].color = (255, 0, 0, 255)  # Set color to red for comparison
             yield
-        self.x[k:r + 1] = left[i:] + right[j:]
-        self.update_bars()
+            self.bars[l + i].color = (255, 255, 255, 255)  # Reset color to white
+            self.bars[mid + 1 + j].color = (255, 255, 255, 255)  # Reset color to white
 
-    def merge(self, l, mid, r):
-        left, right = self.x[l:mid + 1], self.x[mid + 1:r + 1]
-        i, j, k = 0, 0, l
-        while i < len(left) and j < len(right):
             self.x[k] = min((left[i], right[j]))
             if self.x[k] == left[i]:
                 i += 1
@@ -41,11 +36,12 @@ class Renderer(Window):
                 j += 1
             k += 1
             yield
+
         self.x[k:r + 1] = left[i:] + right[j:]
         self.update_bars()
 
     def update_bars(self):
-        self.bars = [Rectangle(100 + e * 100, 100, 80, i * 100, batch=self.batch) for e, i in enumerate(self.x)]
+        self.bars = [Rectangle(100 + e * 100, 60, 60, i * 80, batch=self.batch, color=(255, 255, 255, 255)) for e, i in enumerate(self.x)]
 
     def on_update(self, dt):
         try:
